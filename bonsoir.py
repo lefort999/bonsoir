c1d61c39de54750e7ccb7d56a08549b42276595b
lefort999 <jlefort251@gmail.com>
2025-08-12 17:09:56 +0200

    Update bonsoir.py

--- bonsoir.py
+++ bonsoir.py
@@ -1,11 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask
 import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("bonjour.html")
+    try:
+        with open("bonsoir.txt", "r", encoding="utf-8") as f:
+            contenu = f.read()
+    except Exception as e:
+        contenu = f"Erreur lors de la lecture du fichier : {e}"
+    return f"<html><body><h1>{contenu}</h1></body></html>"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
